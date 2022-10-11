b3ba01fff5291d314de8c0d539e598fed291245f
BriMoreno <BriMoreno232@gmail.com>
2022-10-10 22:55:13 -0700

    added menu and a if else loop for the user selection

--- server.js
+++ server.js
@@ -1,34 +1,60 @@
-const express = require('express');
-const mysql = require('mysql2');
+const mysql = require('mysql');
+const inquirer = require('inquirer');
 
-const PORT = process.env.PORT || 3001;
-const app = express();
+var db = mysql.createConnection({
 
-// Express middleware
-app.use(express.urlencoded({ extended: false }));
-app.use(express.json());
+    host: "localhost",
+    port: 3301,
+    user: "root",
+    password: "Bhjdfv12!@",
+    database: "business_db"
+  });
 
-// Connect to database
-const db = mysql.createConnection(
-  {
-    host: 'localhost',
-    // MySQL username,
-    user: 'root',
-    // MySQL password
-    password: 'Bhjdfv12!@',
-    database: 'business_db'
-  },
-  console.log(`Connected to the business_db database.`)
-);
 
-// delete query
+  db.connect(function(err) {
+    if (err) throw err;
+    menuItems();
+  });
 
-db.query(`DELETE FROM course_names WHERE id = ?`, InputEvent, (err, result) => {
-  if (err) {
-    console.log(err);
+// Creating the menu for the options on the tables, editing/updating
+function menuItems() {
+  inquirer.prompt({
+    name:"action",
+    type:"list",
+    message: "Select an option to continue",
+    choices: [
+      "View Departments",
+      "View Roles",
+      "View Employee List",
+      "Add a Department",
+      "Add a Role",
+      "Add an Employee",
+      "Update Employee Record",
+      "Exit"
+    ]
+  })
+  // if else loop to take to them to were they need and to end connection when they exit
+  .then(function(selection) {
+  if (selection.action === "View Departments"){
+    viewDept();
+  } else if (selection.action === "View Roles"){
+    viewRole();
+  } else if (selection.action === "View Employee List"){
+    viewEmploy();
+  } else if (selection.action === "Add a Department"){
+    addDept();
+  } else if (selection.action === "Add a Role"){
+    addRole();
+  } else if (selection.action === "Add an Employee"){
+    addEmploy();
+  } else if (selection.action === "Update Employee Record"){
+    updateEmploy();
+  } else if (selection.action === "Exit"){
+    db.end();
   }
-  console.log(result);
-});
+
+})
+
 
 
 
@@ -40,3 +66,4 @@ app.use((req, res) => {
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`);
 });
+};
